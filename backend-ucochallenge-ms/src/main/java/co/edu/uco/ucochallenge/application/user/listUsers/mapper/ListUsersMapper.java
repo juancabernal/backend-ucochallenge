7fea4adf8a667a19328a2ff4f52dacb91939fa37
juancabernal <juancamilobernal2222@gmail.com>
2025-10-30 16:06:21 -0500

    Subiendo respaldo

--- backend-ucochallenge-ms/src/main/java/co/edu/uco/ucochallenge/application/user/listUsers/mapper/ListUsersMapper.java
+++ backend-ucochallenge-ms/src/main/java/co/edu/uco/ucochallenge/application/user/listUsers/mapper/ListUsersMapper.java
@@ -9,23 +9,24 @@ import co.edu.uco.ucochallenge.application.user.listUsers.dto.ListUsersOutputDTO
 import co.edu.uco.ucochallenge.application.user.listUsers.dto.ListUsersResponseDTO;
 import co.edu.uco.ucochallenge.domain.user.model.User;
 
+//.
 @Mapper(componentModel = "spring")
 public interface ListUsersMapper {
 
-        @Mapping(target = "userId", source = "id")
-        @Mapping(target = "idType", expression = "java(user.idType().toString())")
-        @Mapping(target = "fullName", expression = "java(buildFullName(user))")
-        ListUsersOutputDTO toOutput(User user);
+    @Mapping(target = "userId", source = "id")
+    @Mapping(target = "idType", expression = "java(user.idType().toString())")
+    @Mapping(target = "fullName", expression = "java(buildFullName(user))")
+    ListUsersOutputDTO toOutput(User user);
 
-        default ListUsersResponseDTO toResponse(final List<User> users) {
-                return new ListUsersResponseDTO(users.stream().map(this::toOutput).toList());
-        }
+    default ListUsersResponseDTO toResponse(final List<User> users) {
+            return new ListUsersResponseDTO(users.stream().map(this::toOutput).toList());
+    }
 
-        default String buildFullName(final User user) {
-                return String.join(" ",
-                                user.firstName(),
-                                user.secondName(),
-                                user.firstSurname(),
-                                user.secondSurname());
-        }
+    default String buildFullName(final User user) {
+            return String.join(" ",
+                            user.firstName(),
+                            user.secondName(),
+                            user.firstSurname(),
+                            user.secondSurname());
+    }
 }
